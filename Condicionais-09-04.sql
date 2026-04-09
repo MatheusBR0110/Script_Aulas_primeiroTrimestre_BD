fa2993cf2aaf6be4f8bdb9c9da8fc650e7443e16
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-04-09 09:48:05 -0300

    Add files via upload

---
+++ Condicionais-09-04.sql
@@ -0,0 +1,32 @@
+select * from tabela_de_clientes;
+select * from itens_notas_fiscais;
+
+select * from notas_fiscais as NF
+inner join itens_notas_fiscais as INF
+on NF.NUMERO = INF.NUMERO;
+
+#Seleção das colunas de interesse
+select NF.CPF, NF.Data_Venda, INF.QUANTIDADE 
+from notas_fiscais as NF
+inner join itens_notas_fiscais as INF
+on NF.NUMERO = INF.NUMERO;
+
+#Manipular a data (ano e mes)
+select NF.CPF,
+date_format(NF.data_venda, '%Y-%M') as data_venda,
+INF.QUANTIDADE
+from notas_fiscais as NF
+inner join itens_notas_fiscais as INF
+on NF.NUMERO = INF.NUMERO;
+
+#Soma para obter a quantidade total
+select NF.CPF,
+date_format(NF.data_venda, '%Y-%M') as data_venda,
+sum(INF.QUANTIDADE) as QUANTIDADE
+from notas_fiscais as NF
+inner join itens_notas_fiscais as INF
+on NF.NUMERO = INF.NUMERO
+group by NF.CPF, DATA_VENDA;
+
+/*Obter o volume de compras da tabela de clientes*/
+select * from tabela_de_clientes;
